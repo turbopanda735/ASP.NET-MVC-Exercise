9fa9944c3230639a39b52a765bd9179b5517deea
Joel <franco4prez@gmail.com>
2022-09-19 11:41:11 -0400

    exercise

---
+++ ASPNET intro/Models/Product/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace ASPNET_intro.Models.Product
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
---
+++ ASPNET intro/Models/Product/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using System.Data;
+
+namespace ASPNET_intro.Models.Product
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
